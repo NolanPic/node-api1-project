5fb03591353d658c9241aa19041e243e7e294e39
Nolan Picini <nolanpic@gmail.com>
2020-02-10 13:26:35 -0800

    Add GET /api/users endpoint

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const DB = require('./data/db.js');
 
 const server = express();
 
+// root request
 server.get('/', (req, res) => {
     res.json('Welcome to Nodejs');
+});
+
+const url = '/api/users';
+
+server.get(url, (req, res) => {
+    DB.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ err });
+    });
 });
 
 const port = 5000;
