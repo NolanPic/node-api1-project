c2cdbfd880b0469d9f7a487a511b7b922f62205a
Nolan Picini <nolanpic@gmail.com>
2020-02-10 13:05:34 -0800

    Run basic server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json('Welcome to Nodejs');
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log(`\nServer listening on port ${port}\n`));
